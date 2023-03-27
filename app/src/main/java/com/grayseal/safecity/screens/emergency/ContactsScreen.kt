24e1bca937345dc70771bd54a906dfcbb74d2469
lynnemunini <muninilynne65@gmail.com>
2023-03-27 18:06:37 +0300

    Emergency contact image

--- app/src/main/java/com/grayseal/safecity/screens/emergency/ContactsScreen.kt
+++ app/src/main/java/com/grayseal/safecity/screens/emergency/ContactsScreen.kt
@@ -1,18 +1,20 @@
 package com.grayseal.safecity.screens.emergency
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -93,49 +95,66 @@ fun ContactsScreen(navController: NavController) {
                                 )
                             },
                     ) {
-                        Column(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Row(modifier = Modifier.fillMaxWidth()) {
-                                Text(
-                                    item.name.capitalize(),
-                                    fontWeight = FontWeight.SemiBold,
+                        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+                            Column(
+                                modifier = Modifier.width(IntrinsicSize.Min)
+                                    .height(IntrinsicSize.Min)
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.emergency),
+                                    contentDescription = "Emergency Contact",
+                                    modifier = Modifier.size(40.dp).clip(CircleShape)
                                 )
                             }
-                            if(pressed) {
-                                Row(modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp)) {
-                                    Text(
-                                        item.description,
-                                    )
-                                }
-                            }
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                verticalAlignment = Alignment.CenterVertically
+                            Column(
+                                modifier = Modifier
+                                    .padding(2.dp),
+                                verticalArrangement = Arrangement.spacedBy(3.dp)
                             ) {
-                                Column(modifier = Modifier.weight(1f)) {
+                                Row(modifier = Modifier.fillMaxWidth()) {
                                     Text(
-                                        item.contact.substring(4),
-                                        fontWeight = FontWeight.Medium
+                                        item.name.capitalize(),
+                                        fontWeight = FontWeight.SemiBold,
                                     )
                                 }
-                                Column(modifier = Modifier.weight(1f)) {
+                                if (pressed) {
                                     Row(
-                                        modifier = Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.End,
-                                        verticalAlignment = Alignment.CenterVertically
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(vertical = 5.dp)
                                     ) {
-                                        Icon(
-                                            painter = painterResource(id = R.drawable.ic_call),
-                                            contentDescription = "Call",
-                                            tint = Green,
-                                            modifier = Modifier.padding(8.dp)
+                                        Text(
+                                            item.description,
                                         )
-                                        Text("Call now")
+                                    }
+                                }
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Column(modifier = Modifier.weight(1f)) {
+                                        Text(
+                                            item.contact.substring(4),
+                                            fontWeight = FontWeight.Medium
+                                        )
+                                    }
+                                    Column(modifier = Modifier.weight(1f)) {
+                                        Row(
+                                            modifier = Modifier.fillMaxWidth(),
+                                            horizontalArrangement = Arrangement.End,
+                                            verticalAlignment = Alignment.CenterVertically
+                                        ) {
+                                            Icon(
+                                                painter = painterResource(id = R.drawable.ic_call),
+                                                contentDescription = "Call",
+                                                tint = Green,
+                                                modifier = Modifier.padding(8.dp)
+                                            )
+                                            Text("Call now")
+                                        }
                                     }
                                 }
                             }
-                            Divider()
                         }
                     }
                 }
